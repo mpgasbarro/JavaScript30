4316b40bd359c649f2582d86e4f44a370e66d23f
Gaspy <mpgasbarro@gmail.com>
2020-12-08 10:35:06 -0500

    have full funcationality working with the keys showing light and playing sound

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,23 +1,22 @@
- function removeTransition(e) {
-     console.log(e.propertyName);
-     let keys = document.querySelector(`div[data-key="${e.keyCode}"]`);
-     if(keys.classList.contains("playing")){
-         keys.classList.remove("playing")
-     }
- }
+function removeTransition(e) {
+	console.log(e.propertyName);
+	if (e.propertyName !== 'transform') return;
+	e.target.classList.remove('playing');
+}
 
 playSound = (e) => {
-    let key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-   
-    if (!audio) return;
+	let key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+	let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 
-        key.classList.add('playing');
-        audio.currentTime = 0
-        audio.play();
-        
-} 
+	if (!audio) return;
 
+	key.classList.add('playing');
+	audio.currentTime = 0;
+	audio.play();
+};
 
-document.addEventListener("keydown", playSound)
-document.addEventListener("keydown", removeTransition)
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach((key) => key.addEventListener('transitionend', removeTransition));
+
+document.addEventListener('keydown', playSound);
+document.addEventListener('keydown', removeTransition);
